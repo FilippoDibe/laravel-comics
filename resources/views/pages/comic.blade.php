ed10e7c448870bb269873001d1eb5d38147d259b
filippo cafaro <144951463+FilippoDibe@users.noreply.github.com>
2024-02-11 01:17:55 +0100

    end template page comic

--- resources/views/pages/comic.blade.php
+++ resources/views/pages/comic.blade.php
@@ -28,8 +28,53 @@
             {{-- il blocco con l'immagine con il codice sconto  --}}
         </div>
     </div>
-    <div class="spec-container">
-        <h2>talent</h2>
-        <h2>specs</h2>
+    <div class="container">
+        <div class="details-container">
+            <div class="talent">
+                <h3>Talent</h3>
+                <div class="block">
+                    <div class="line"></div>
+
+                    <span class="title">Art by:</span>
+                    @foreach ($comic['artists'] as $artist)
+                        <span class="names">{{ $artist }}</span>
+                    @endforeach
+                </div>
+
+                <div class="block" id="written">
+                    <div class="line"></div>
+                    <span class="title">Written by:</span>
+                    @foreach ($comic['writers'] as $writer)
+                        <span class="names">{{ $writer }}</span>
+                    @endforeach
+                </div>
+            </div>
+
+        </div>
+        <div class="details-container">
+            <div class="specs">
+                <h3>Specs</h3>
+                <div class="line"></div>
+                <div class="block-spec">
+                    <div class="title">Series:</div>
+                    <div class="specs-info">{{ $comic['series'] }}</div>
+                </div>
+                <div class="line"></div>
+                <div class="block-spec">
+                    <div class="title">U.S. Price:</div>
+                    <div>{{ $comic['price'] }}</div>
+                </div>
+                <div class="line"></div>
+                <div class="block-spec" id="last-spec">
+                    <div class="title">On Sale Date:</div>
+                    <div>{{ \Carbon\Carbon::parse($comic['sale_date'])->format('M d Y') }}</div>
+                </div>
+                <div class="line"></div>
+
+
+
+
+            </div>
+        </div>
     </div>
 @endsection
